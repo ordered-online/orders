bea449bd635b66d23b179055caa0d3a09185391c
PhilippMatthes <github@philippmatth.es>
2019-10-29 14:26:03 +0100

    Remove price field from order

--- orders/orders/models.py
+++ orders/orders/models.py
@@ -50,10 +50,6 @@ class Order(models.Model):
 
     # mandatory fields
     product_id = models.IntegerField()
-    # note that the price could also be inferred by the product,
-    # but we want a fixed price on purchase, independent from the
-    # price of the product itself
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
 
     class Meta:
