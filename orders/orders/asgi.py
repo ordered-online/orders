0c05388ed44e47fb735e0ad71c1a2289a83bc956
Felix Kästner <kaestner.fe@gmail.com>
2019-11-09 20:00:32 +0100

    add asgi setup for daphne

---
+++ orders/orders/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for orders project.
+
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+
+For more information on this file, see
+https://channels.readthedocs.io/en/latest/deploying.html
+"""
+
+import os
+import django
+
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orders.settings")
+
+django.setup()
+
+application = get_default_application()
