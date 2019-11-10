91a8a2d3cd7bcc4544cbf557d5f40aaca002211f
Felix Kästner <kaestner.fe@gmail.com>
2019-11-10 01:37:32 +0100

    get redis host from environment variable rather than hardcoded

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -78,7 +78,9 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)]
+            'hosts': [
+                (os.environ.get('REDIS_HOST', default='127.0.0.1'), int(os.environ.get('REDIS_PORT', default=6379)))
+            ]
         }
     }
 }
